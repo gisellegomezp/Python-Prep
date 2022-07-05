a492930624a6e919afb5bed15ef2a96f220b8303
Andy <48845906+andykot@users.noreply.github.com>
2022-07-05 20:48:10 -0300

    Check point committed

--- 01 - Introducción a la Programación/Practica.py
+++ 01 - Introducción a la Programación/Practica.py
@@ -1,1 +1,8 @@
-print('Hola Mundo!222')
+def EsPrimo(valor):
+    if (type(valor) != int):
+            return None
+    for i in range(2, (int(valor / 2) + 1)):
+        if valor % i == 0:
+            return False
+    return True
+EsPrimo(11)
